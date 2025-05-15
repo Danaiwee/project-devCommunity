ffea7c391c958953df07fdb55f55fab1ac887af8
DanaiWee <Danai_wee@hotmail.com>
2025-05-15 13:27:26 +0700

    Implment the UI of the Question Form

---
+++ components/forms/QuestionForm.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { AskQuestionSchema } from "@/lib/validation";
+
+import Editor from "../editor";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+const QuestionForm = () => {
+  const form = useForm<z.infer<typeof AskQuestionSchema>>({
+    resolver: zodResolver(AskQuestionSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+      tags: [],
+    },
+  });
+
+  const handleCreateQuestion = async () => {};
+
+  return (
+    <Form {...form}>
+      <form
+        className="flex w-full flex-col gap-10"
+        onSubmit={form.handleSubmit(handleCreateQuestion)}
+      >
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className="flex flex-col w-full">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Question Title
+                <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input
+                  className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription className="body-regular mt-1 text-light-500">
+                Be specific and imagine you&lsquo;re asking a question to
+                another person.
+              </FormDescription>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem className="flex flex-col w-full">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Detailed explanation of your problem{" "}
+                <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Editor />
+              </FormControl>
+              <FormDescription className="body-regular mt-1 text-light-500">
+                Introduce the problem and expand on what you&lsquo;ve put in the
+                title
+              </FormDescription>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="tags"
+          render={({ field }) => (
+            <FormItem className="flex flex-col w-full">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Tags
+                <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <div>
+                  <Input
+                    className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border"
+                    {...field}
+                  />
+                  Tags
+                </div>
+              </FormControl>
+              <FormDescription className="body-regular mt-1 text-light-500">
+                Add up to 3 tags to describe what your question is about. You
+                need to press entoer to add a tag.
+              </FormDescription>
+            </FormItem>
+          )}
+        />
+
+        <div className='mt-16 flex justify-end'>
+          <Button type='submit' className='primary-gradient w-fit !text-light-900'>
+            Ask a Question
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default QuestionForm;
