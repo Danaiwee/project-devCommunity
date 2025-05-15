466e4dd61f8542c2811eabf51f8f96c136977f2c
DanaiWee <Danai_wee@hotmail.com>
2025-05-15 19:43:09 +0700

    Implement Collection model

---
+++ database/collection.model.ts
@@ -0,0 +1,18 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface ICollection {
+    author: Types.ObjectId;
+    question: Types.ObjectId;
+}
+
+export interface ICollectionDoc extends ICollection, Document {}
+
+const collectionSchema = new Schema({
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  question: { type: Schema.Types.ObjectId, ref: "Question", required: true },
+});
+
+const Collection =
+  models?.Collection || model<ICollection>("Collection", collectionSchema);
+
+export default Collection;
