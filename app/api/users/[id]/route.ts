89986a27957976c2f673b827fe3d6caeac2e799b
DanaiWee <Danai_wee@hotmail.com>
2025-05-16 15:02:43 +0700

    Implement Dynamic User Routes

---
+++ app/api/users/[id]/route.ts
@@ -0,0 +1,78 @@
+import { NextResponse } from "next/server";
+
+import User from "@/database/user.model";
+import handleError from "@/lib/handler/error";
+import { NotFoundError, ValidationError } from "@/lib/http-error";
+import dbConnect from "@/lib/mongoose";
+import { UserSchema } from "@/lib/validation";
+import { APIErrorResponse } from "@/types/global";
+
+// Get user by id
+export async function GET(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  if (!id) throw new NotFoundError("User id");
+
+  try {
+    await dbConnect();
+
+    const user = await User.findById(id);
+    if (!user) throw new NotFoundError("User");
+
+    return NextResponse.json({ success: true, data: user }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+// Delete user by id
+export async function DELETE(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  if (!id) throw new NotFoundError("User id");
+
+  try {
+    await dbConnect();
+
+    const user = await User.findByIdAndDelete(id);
+    if (!user) throw new NotFoundError("User");
+
+    return NextResponse.json({ success: true, data: user }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+// Update user by Id
+export async function PUT(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  if (!id) throw new NotFoundError("User id");
+
+  try {
+    await dbConnect();
+
+    const body = await request.json();
+    const validateData = UserSchema.partial().safeParse(body);
+
+    if (!validateData.success)
+      throw new ValidationError(validateData.error.flatten().fieldErrors);
+
+    const updatedUser = await User.findByIdAndUpdate(id, validateData.data, {
+      new: true,
+    });
+
+    return NextResponse.json(
+      { success: true, data: updatedUser },
+      { status: 200 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
