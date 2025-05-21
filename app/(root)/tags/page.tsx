081c1b85d2a34bf8f4700f3ba4d5e9f16a9878a0
DanaiWee <Danai_wee@hotmail.com>
2025-05-21 14:01:29 +0700

    Implement getTag server action

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import { getTags } from "@/lib/actions/tag.action";
 
-const Tags = () => {
+
+const Tags = async ({searchParams}: RouteParams) => {
+  const {page, pageSize, query, filter} = await searchParams;
+
+  const {success, data, errors} = await getTags({
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+    filter,
+    query: query || "javascript"
+  });
+
   return (
     <div>Tags</div>
   )
