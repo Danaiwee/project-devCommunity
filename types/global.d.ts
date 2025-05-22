7786b6d1f4f34ec1407cb1eceef4e0167c9cea9c
DanaiWee <Danai_wee@hotmail.com>
2025-05-22 17:15:30 +0700

    Implement get all users actions

--- types/global.d.ts
+++ types/global.d.ts
@@ -67,4 +67,16 @@ declare global {
     hasupVoted: boolean;
     hasdownVoted: boolean;
   }
+
+  interface User {
+    _id: string;
+    name: string;
+    username: string;
+    email: string;
+    bio?: string;
+    image?: string;
+    location?: string;
+    portfolio?: string;
+    reputation?: number;
+  }
 }
