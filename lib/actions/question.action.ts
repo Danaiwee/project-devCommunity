6bad819354775827d7c155c2a24248849f4a9095
DanaiWee <Danai_wee@hotmail.com>
2025-05-26 09:46:26 +0700

    Implement get Recommended question

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,10 +1,11 @@
 "use server";
 
-import mongoose, { FilterQuery } from "mongoose";
+import mongoose, { FilterQuery, Types } from "mongoose";
 import { revalidatePath } from "next/cache";
 import { after } from "next/server";
 
-import { Answer, Collection, Vote } from "@/database";
+import { auth } from "@/auth";
+import { Answer, Collection, Interaction, Vote } from "@/database";
 import Question from "@/database/question.model";
 import TagQuestion from "@/database/tag-question";
 import Tag, { ITagDoc } from "@/database/tag.model";
@@ -14,6 +15,7 @@ import {
   GetQuestionParams,
   IncrementViewsParams,
   DeleteQuestionParams,
+  RecommendationParams,
 } from "@/types/action";
 
 import action from "../handler/action";
@@ -213,6 +215,70 @@ export async function editQuestion(
   }
 }
 
+export async function getRecommendedQuestion({
+  userId,
+  query,
+  skip,
+  limit,
+}: RecommendationParams) {
+  // 1.Find interaction with userId
+  const interactions = await Interaction.find({
+    user: new Types.ObjectId(userId),
+    actionType: "question",
+    action: { $in: ["view", "upvote", "bookmark", "post"] },
+  })
+    .sort({ createdAt: -1 })
+    .limit(50)
+    .lean();
+
+  // 2.Find an array of question id from interactions []
+  const interactedQuestionIds = interactions.map(
+    (interaction) => interaction.actionId
+  );
+
+  // 3.Find questions from question id []
+  const interactionQuestions = await Question.find({
+    _id: { $in: interactedQuestionIds },
+  }).select("tags");
+
+  // 4.Find tags id from questions []
+  const allTags = interactionQuestions.flatMap((question) =>
+    question.tags.map((tag: Types.ObjectId) => tag.toString())
+  );
+
+  // 5.Make Tag id unique []
+  const uniqueTagIds = [...new Set(allTags)];
+
+  // 6.Set up the query options
+  const recommendedQuery: FilterQuery<typeof Question> = {
+    _id: { $nin: interactedQuestionIds },
+    author: { $ne: new Types.ObjectId(userId) },
+    tags: { $in: uniqueTagIds.map((id) => new Types.ObjectId(id)) },
+  };
+
+  if (query) {
+    recommendedQuery.$or = [
+      { title: { $regex: query, options: "i" } },
+      { content: { $regex: query, $options: "i" } },
+    ];
+  }
+
+  const totalQuestions = await Question.countDocuments(recommendedQuery);
+
+  const questions = await Question.find(recommendedQuery)
+    .populate("tags", "name")
+    .populate("author", "name image")
+    .sort({ upvotes: -1, views: -1 })
+    .skip(skip)
+    .limit(limit)
+    .lean();
+
+  return {
+    questions: JSON.parse(JSON.stringify(questions)),
+    isNext: totalQuestions > skip + questions.length,
+  };
+}
+
 export async function getQuestion(
   params: GetQuestionParams
 ): Promise<ActionResponse<Question>> {
@@ -259,10 +325,6 @@ export async function getQuestions(
 
   const filterQuery: FilterQuery<typeof Question> = {};
 
-  if (filter === "recommended") {
-    return { success: true, data: { questions: [], isNext: false } };
-  }
-
   if (query) {
     filterQuery.$or = [
       { title: { $regex: new RegExp(query, "i") } },
@@ -289,6 +351,27 @@ export async function getQuestions(
   }
 
   try {
+    if (filter === "recommended") {
+      const session = await auth();
+      const userId = session?.user?.id;
+
+      if (!userId) {
+        return { success: true, data: { questions: [], isNext: false } };
+      }
+
+      const recommended = await getRecommendedQuestion({
+        userId,
+        query,
+        skip,
+        limit,
+      });
+
+      return {
+        success: true,
+        data: recommended,
+      };
+    }
+
     const totalQuestions = await Question.countDocuments(filterQuery);
 
     const questions = await Question.find(filterQuery)
